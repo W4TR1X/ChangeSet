f38642a681be4cb41465816fff0111d06f8883f4
m4TR1x <mustafa.dagci@outlook.com.tr>
2025-03-09 12:39:07 +0300

    changes

--- build-windows.ps1
+++ build-windows.ps1
@@ -1,4 +1,4 @@
-dotnet publish src/ChangeSet/ChangeSet.csproj -c Release -o ../dist/win-x64/ -r win-x64 -p:PublishSingleFile=true -p:PublishTrimmed=true --self-contained true /p:DebugType=None /p:IncludeNativeLibrariesForSelfExtract=true
+dotnet publish src/ChangeSet/ChangeSet.csproj -c Release -o dist/win-x64/ -r win-x64 -p:PublishSingleFile=true -p:PublishTrimmed=true --self-contained true /p:DebugType=None /p:IncludeNativeLibrariesForSelfExtract=true
 
 if ($LASTEXITCODE -ne 0) {
     exit $LASTEXITCODE
