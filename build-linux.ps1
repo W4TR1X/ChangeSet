1e9c5ab8a9027489df6891e0cf1e5860b5e42657
m4TR1x <mustafa.dagci@outlook.com.tr>
2025-03-09 16:27:29 +0300

    changes

--- build-linux.ps1
+++ build-linux.ps1
@@ -1,7 +1,7 @@
 # Call Sample
 # sed -i 's/\r//g' build-linux.ps1 && pwsh -File build-linux.ps1
 
-dotnet publish src/ChangeSet/ChangeSet.csproj -v diag -c Release -o ./dist/linux-x64/ -r linux-x64 -p:PublishSingleFile=true -p:PublishTrimmed=true --self-contained true /p:DebugType=None /p:IncludeNativeLibrariesForSelfExtract=true
+dotnet publish src/ChangeSet/ChangeSet.csproj -c Release -o ./dist/linux-x64/ -r linux-x64 -p:PublishSingleFile=true -p:PublishTrimmed=true --self-contained true /p:DebugType=None /p:IncludeNativeLibrariesForSelfExtract=true
 
 if ($LASTEXITCODE -ne 0) {
     exit $LASTEXITCODE
