a3c1d458ee3c78ecf67ec9723201939517ff67ec
m4TR1x <mustafa.dagci@outlook.com.tr>
2025-03-11 21:22:23 +0300

    changes

--- index.js
+++ index.js
@@ -5,16 +5,16 @@ const ospath = require('path');
 
 try {
   // GitHub Actions inputlarını al
-  const path = core.getInput('path') ?? ".";
-  const config = core.getInput('config') ?? './changeset.config.json';
+  const path = core.getInput('path') || process.env.GITHUB_WORKSPACE;
+  const config = core.getInput('config') || ospath.join(process.env.GITHUB_WORKSPACE, 'changeset.config.json');
 
   const pairsString = core.getInput('pairs');
-  const pairs = pairsString.split('\n').map(item => item.trim()).filter(Boolean);  
+  const pairs = pairsString.split('\n').map(item => item.trim()).filter(Boolean);
   const joinedPairs = pairs.join(' ');
 
   // PowerShell betiğini parametrelerle çalıştır
   execSync(`pwsh -File "${ospath.join(__dirname, 'run-action.ps1')}" -path "${path}" -config "${config}" -pairs ${joinedPairs}`,
-    { stdio: 'inherit', shell: "pwsh" }
+    { stdio: 'inherit', shell: "pwsh", cwd: process.env.GITHUB_WORKSPACE }
   );
 }
 catch (err) {
